746cd96486520e95b3e2d912f1b6593f75cc2f53
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2026-04-27 18:04:19 +0700

    Memasukan Nilai 2

--- pointersatu.cpp
+++ pointersatu.cpp
@@ -12,3 +12,7 @@ class mahasiswa{
 int main (){
     mahasiswa mhs{1}; // Object mhs
     mhs.showNim(); //Member Access Operator
+
+    mahasiswa &refMhs = mhs; //Pointer Reference refMhs
+    refMhs.nim = 2; //Member Access Operator
+    mhs.showNim();
