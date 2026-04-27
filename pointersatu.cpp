3d4d9b82aa684bdf17c88c39b43c4f6e28065634
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2026-04-27 18:03:57 +0700

    Memasukan nilai 1

--- pointersatu.cpp
+++ pointersatu.cpp
@@ -7,3 +7,8 @@ class mahasiswa{
     void showNim(){
         cout<<"No Induk = "<<nim<<endl;
     }
+};
+
+int main (){
+    mahasiswa mhs{1}; // Object mhs
+    mhs.showNim(); //Member Access Operator
