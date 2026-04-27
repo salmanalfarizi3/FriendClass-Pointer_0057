758a7d7a5479f0786270c08838a0382648da6fd1
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2026-04-27 18:37:28 +0700

    class mahasiswa dengan modifer public nim dan fungsi shownim dan menampilkan No induk = nim

--- pointerdua.cpp
+++ pointerdua.cpp
@@ -3,4 +3,11 @@ using namespace std;
 
 class mahasiswa 
 {
-  
+    public:
+        int nim;
+        void showNim() 
+        {
+            cout << "No Induk = " << nim << endl;
+        }
+};
+
